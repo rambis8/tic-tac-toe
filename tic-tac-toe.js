e83d0f0bbe9c9c06b919e2af1731e859fdc441b9
rambis8 <rik_1991@hotmail.com>
2024-09-10 22:17:06 +0200

    comments voor todo.

---
+++ tic-tac-toe.js
@@ -0,0 +1,18 @@
+/* Gameboard object met array
+factory inside an IIFE (module pattern) */
+// speelbord is een grid van 9 elementen, dus arraylist
+// Waarbij elementen corresponderen met een 3x3 grid:
+// 0 t/m 2 1e rij van 3
+// 3 t/m 5 2e rij van 3
+// 6 t/m 8 3e rij van 3
+
+/**
+ * Player object
+ */
+// Er zijn 2 spelers, speler X en speler O (oftewel kruisje en rondje)
+// uitendelijk krijgen de players ook een naam attribuut
+
+
+// Object voor controle van de flow van de game
+// Noemen we het Game object
+// Dit object 
