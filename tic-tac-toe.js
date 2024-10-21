60cf0a04fe3b6caed7e6a211caf1b84ad8118500
rambis8 <rik_1991@hotmail.com>
2024-10-21 11:36:46 +0200

    wip.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,18 +1,44 @@
-/* Gameboard object met array
-factory inside an IIFE (module pattern) */
-// speelbord is een grid van 9 elementen, dus arraylist
-// Waarbij elementen corresponderen met een 3x3 grid:
-// 0 t/m 2 1e rij van 3
-// 3 t/m 5 2e rij van 3
-// 6 t/m 8 3e rij van 3
+const winconditions = [[0,1,2], [3,4,5], [6,7,8] [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
-/**
- * Player object
- */
-// Er zijn 2 spelers, speler X en speler O (oftewel kruisje en rondje)
-// uitendelijk krijgen de players ook een naam attribuut
+// methode om winnaar vast te stellen:
+// per winconditie controleren of binnen het gameboard de drie posities dezelfde waarde bevatten
+// als het resultaat true is is er een winnaar. als het resultaat false is en de array is vol
+// is het gelijkspel (tie).
+
+function Gameboard() {
+    const gameboard = new Array(9);
+    this.placeMarker = function(Player, position) {
+        console.log(this.gameboard);
+        if (this.gameboard[position] !== null) {
+            console.log('This position has been taken already.')
+            return;
+        }
+        this.gameboard[position] = Player.marker;
+    } 
+}
+
+function Player(name, marker) {
+    this.name = name;
+    this.marker = marker;
+}
+
+console.log('How to play this game?');
+console.log('First, create the first player using this command: const player1 = new Player("name", "marker")');
+console.log('Second, create the second player using this command: const player2 = new Player("name", "marker")');
+console.log('Where name and marker should be chosen by each player.')
 
 
-// Object voor controle van de flow van de game
-// Noemen we het Game object
-// Dit object 
+
+// spelers mogen om beurten een vakje vullen
+// speler X begint en daarna mag speler O
+// Zodra de array groter is dan 4 (dus 5 entries)
+// moet er gecontroleerd worden of er aan een win conditie is voldaan voor 1 van beide spelers
+// dit gaat op de volgende methode:
+
+
+// TODO
+
+// UI
+// input name for players
+// button to start/restart the game
+// display score
